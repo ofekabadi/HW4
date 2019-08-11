fe91d82f8a598f41c90312efba132da47993c39f
ofekabadi <50488447+ofekabadi@users.noreply.github.com>
2019-08-11 20:29:01 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,141 @@
+//
+// Created by Adam on 06-Aug-19.
+//
+//
+// Created by RomG on 29/06/2019.
+//
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include "Tree.h"
+
+using std::string;
+using std::pair;
+using std::make_pair;
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::logic_error;
+using std::cin;
+using std::runtime_error;
+
+typedef enum {NEW=0, INSERT, DELETE, SEARCH, EMPTY, SIZE, TRAVEL, QUIT} CommandsEnum;
+const string consoleCommands[] = {"new", "insert", "deleteLeaf", "search", "empty",
+                                  "size", "inorderTraversal", "quit"};
+
+typedef enum {INT=0, FLOAT, STRING} TreeTypes;
+const string treeTypes[] = {"int", "float", "string"};
+
+typedef enum {KEY, ITEM} TypesLocationErrors;
+const string typesErrorLocation[] = {"Key", "Item"};
+
+const string EMPTY_TREE("empty");
+const string NOT_EMPTY_TREE("not ");
+const string FOUND = "found:";
+
+void errorThrower(TypesLocationErrors loc, const string& erroredType){
+    throw runtime_error("Bad " + typesErrorLocation[loc] + " Type: " + erroredType);
+}
+
+template<typename Type1>
+Type1 getInputs(CommandsEnum command){
+    Type1 input;
+    cin >> input;
+    if (cin.fail()){
+        // insert your code here
+    }
+    return input;
+}
+
+template<typename Type1, typename Type2>
+pair<Type1, Type2> getInputs(CommandsEnum command){
+    Type1 firstInput(getInputs<Type1>(command));
+    Type2 secondInput(getInputs<Type2>(command));
+    return make_pair(firstInput, secondInput);
+}
+
+template <typename T, typename G>
+void treeSession() {
+    Tree<T, G> tree;
+    string input;
+    cin >> input;
+    while (input != consoleCommands[QUIT]){
+        if (input == consoleCommands[INSERT]) {
+            try{
+                pair<T,G> tempPair(getInputs<T, G>(INSERT));
+                T key(tempPair.first);
+                G item(tempPair.second);
+                tree.insert(key, item);
+            }
+            catch (const logic_error& error){
+                cerr << error.what() << endl;
+            }
+        }
+        else if (input == consoleCommands[DELETE]){
+            // insert your code here
+
+        }
+        else if (input == consoleCommands[SEARCH]){
+            // insert your code here
+        }
+        else if (input == consoleCommands[EMPTY]){
+            // insert your code here
+        }
+        else if (input == consoleCommands[SIZE]){
+            // insert your code here
+        }
+        else if (input == consoleCommands[TRAVEL]){
+            // insert your code here
+        }
+        else {
+            cerr << "Bad command" << endl;
+        }
+        cin >> input;
+    }
+}
+
+template <typename T>
+void treeCreator(){
+    string valueType;
+    cin >> valueType;
+    if (valueType == treeTypes[INT]){
+        // insert your code here
+    }
+    else if (valueType == treeTypes[FLOAT]){
+        // insert your code here
+    }
+    else if (valueType == treeTypes[STRING]){
+        // insert your code here
+    }
+    else{
+        // insert your code here
+    }
+}
+
+int main() {
+    try{
+        string input;
+        cin >> input;
+        if (input == consoleCommands[NEW]) {
+            // insert your code here
+            if (keyType == treeTypes[INT]) {
+                // insert your code here
+            }
+            else if (keyType == treeTypes[FLOAT]){
+                // insert your code here
+            }
+            else if (keyType == treeTypes[STRING]){
+                // insert your code here
+            }
+            else{
+                errorThrower(KEY, keyType);
+            }
+        }
+        else {
+            // insert your code here
+        }
+    }
+    catch( const runtime_error& error){
+        cerr << error.what() << endl;
+    }
+}
